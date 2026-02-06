a130602755de0bb1495210d382049a21779d5f75
muhai <muhai.li@tuya.com>
2026-02-06 16:02:27 +0800

    chore: update

--- src/tools/filesystem/index.ts
+++ src/tools/filesystem/index.ts
@@ -72,7 +72,6 @@ const SearchFilesArgsSchema = z.object({
 const GetFileInfoArgsSchema = z.object({
   path: z.string(),
 });
-console.log('🚀 ~ index ~ zodObjectToJsonSchema(ListDirectoryWithSizesArgsSchema,):', zodObjectToJsonSchema(ListDirectoryWithSizesArgsSchema, 'ListDirectoryWithSizesArgsSchema'));
 
 export const getFilesystemTools = (targetDir: string) => {
   setAllowedDirectories([targetDir]);
